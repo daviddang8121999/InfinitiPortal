40d66215c0b527645732e0b2468762d3def31e1d
David <daviddang8121999@gmail.com>
2023-11-03 14:48:37 +1100

    Header components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignIn from './Pages/SignIn';
+import Header from './Components/Header';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import SignUp from './Pages/SignUp';
+import Profile from './Pages/Profile';
+
 
 
 export default function App() {
-  return <BrowserRouter>
-  <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/sign-in' element={<SignIn />} />
-    <Route path='/sign-up' element={<SignUp />} />
-    <Route path='/about' element={<About />} />
-    <Route path='/profile' element={<Profile />} />
-  </Routes>
+  return (
+  <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
   </BrowserRouter>
+  );
 }
