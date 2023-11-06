11c478ce4a3dbdcca95eb7cd22cacf017dbdc50f
David <daviddang8121999@gmail.com>
2023-11-06 11:28:43 +1100

    complete sign up function

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to='/'>
                 <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                    <span classname='text-slate-500'>Infiniti</span>
+                    <span className='text-slate-500'>Infiniti</span>
                     <span className='text-red-500'>Corporate</span>
                 </h1>
             </Link>
